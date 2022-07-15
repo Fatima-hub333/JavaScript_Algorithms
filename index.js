e0bd56fa49027f5b20afa49258e7ea0db5080468
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-15 20:53:17 +0500

    solve binary tree preorder in iterative way

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ function factorialize(number1){
     }
 }
 factorialize(5);
+
+// Find longest word in a string
+function findLongestWord(stringVal) {
+  let arrayOfStrings = stringVal.split(" ");
+  let longestString = '';
+
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    if (longestString.length < arrayOfStrings[i].length) {
+      longestString = arrayOfStrings[i];
+    }
+  }
+  console.log(longestString)
+}
+
+findLongestWord('The Force is strong in this one');
+
+
+// Binary TREE PREORDER IN ITERATIVE WAY
+var preorderTraversal = function (root) {
+  if (!root) return [];
+  const stack = [root];
+  const result = [];
+
+  while (stack.length) {
+    let node = stack.pop();
+
+    result.push(node.val);
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  return result;
+}
